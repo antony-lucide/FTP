1b3b836badd3ccb5dbbcd00f847020aadf19de7a
Antony <antony.lucide@laplateforme.io>
2022-10-21 15:17:37 +0200

    Ceci est donc mon travail sur la FTP

--- Job_7/Machine.sh
+++ Job_7/Machine.sh
@@ -7,4 +7,7 @@ cat  proftpd.conf > /etc/proftpd/proftpd.conf
 
 cat proftpd.tlc  > /etc/proftpd/proftpd.conf
 
-ssh root@Debian > 'bash-s' < Machine.sh
+openssl genrsa -out /etc/ssl/private/proftpd.key 2048
+
+openssl req -new -x509 -days 3650 -key /etc/ssl/private/proftpd.key -out /etc/ssl/certs/proftpd.crt
+
--- Job_9/Profil.sh
+++ Job_9/Profil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Fichier_CSV=$1
-while IFS="bh" read -r  Prenom MDP Role 
+while IFS="," read -r  Prenom MDP Role 
 do
    sudo useradd -m -p "$MDP" "$Prenom" "$Role" "$Role"
 if [[$Role="admin"]]
@@ -9,6 +9,6 @@ then
     sudo usermod -g sudo "$Prenom" "$Nom" "$Role" "$MDP"
  elif[["$Role"]]
 fi
-done < <(tail -n +2 $Fichier_CSV
+done < <(tail -n +2 $Fichier_CSV)
 
 
