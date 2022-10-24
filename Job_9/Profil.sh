07381f96d0065166c273e8807abdc17e635bc29f
Antony <antony.lucide@laplateforme.io>
2022-10-24 10:08:04 +0200

    Ceci est donc mon travail sur la FTP

--- Job_9/Profil.sh
+++ Job_9/Profil.sh
@@ -4,10 +4,9 @@ Fichier_CSV=$1
 while IFS="," read -r  Prenom MDP Role 
 do
    sudo useradd -m -p "$MDP" "$Prenom" "$Role" "$Role"
-if [[$Role="admin"]]
+if [[ $Role="admin" ]]
 then
     sudo usermod -g sudo "$Prenom" "$Nom" "$Role" "$MDP"
- elif[["$Role"]]
 fi
 done < <(tail -n +2 $Fichier_CSV)
 
